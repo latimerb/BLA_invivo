29d6555c9a50882da63289aae1be77a5efa6c735
Ben Latimer <latimerb@comet-ln2.sdsc.edu>
2019-03-21 08:47:49 -0700

    changed back to 101 files

--- batch_comet.sh
+++ batch_comet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --partition development
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --partition compute
+#SBATCH --nodes=5
+#SBATCH --ntasks-per-node=20
 #SBATCH -A TG-DBS180005
 #SBATCH --job-name=ca1
 #SBATCH --output=ca1%j.out
