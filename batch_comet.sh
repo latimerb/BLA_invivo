f47308d45023f6ae7e46eeade63cff49a745ff90
Ben Latimer <latimerb@login1.stampede2.tacc.utexas.edu>
2019-03-22 16:51:41 -0500

    added min electrode distance

--- batch_comet.sh
+++ batch_comet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --partition compute
+#SBATCH --partition normal
 #SBATCH --nodes=5
 #SBATCH --ntasks-per-node=20
 #SBATCH -A TG-DBS180005
 #SBATCH --job-name=ca1
 #SBATCH --output=ca1%j.out
-#SBATCH --time 0-00:30
+#SBATCH --time 0-01:00
 #SBATCH --qos=normal
 
 
